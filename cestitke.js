4734c4ee317b00f8d7a7f8d95192c67c11cce584
Hypersensitive <38589867+Hypersensitive@users.noreply.github.com>
2022-12-02 00:34:19 +0100

    Add files via upload

---
+++ cestitke.js
@@ -0,0 +1,11 @@
+
+ 
+   var cestitke =
+   [
+    {"id":1,"ime":"Luka Gligorević","cestitka":"Srecan rodjendan!"},
+    {"id":2,"ime":"Katarina Trbojević","cestitka":"Tomice gej(m) masteru, srećan ti 25. rođendan!<br/> Sada si na 25.levelu svog života i ide ti sasvim solidno.<br/> Želim ti da u narednoj godini dodatno buffuješ sve svoje skillse i da naletiš na mnogo pogodnih prilika koje će ti dati BIG vetar u leđa da kreneš putem koji si sam sebi zacrtao.<br/> Isto tako želim ti puno hrabrosti i sreće kada naletiš na bilo kakav obstacle i jako jako puno progresa i character developmenta.<br/> A za to su ti potrebni i odgovarajući klanovi ljudi koji će te podržavati u svakoj tvojoj zamisli.<br/> Želim da ti ljudi u koje ulažeš vreme i energiju guraju na vrh i da uvek umeju da ti naprave na licu osmeh.<br/> Uvek će biti momenata kada deluje kao da stvari stagniraju ali u tim momentima ne zaboravi da su ovi ljudi upravo tu za tebe 🧡 Želim da ti naredna godina bude godina puna novih XP-jeva i ona godina koje se sećaš sa osmehom i zbog koje ćeš biti ponosan na sebe.<br/> A, sada go and reach your full potential!!! ❤❤❤From the 💜,SkinnyChubbyBunny"}
+   
+   ]
+
+ 
+
