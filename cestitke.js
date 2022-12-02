ed0b4ff48ff90cd8bff4db1187806e7c7d3ebfab
Hypersensitive <38589867+Hypersensitive@users.noreply.github.com>
2022-12-02 13:33:50 +0100

    Add files via upload
    Signed-off-by: Hypersensitive <38589867+Hypersensitive@users.noreply.github.com>

--- cestitke.js
+++ cestitke.js
@@ -2,10 +2,170 @@
  
    var cestitke =
    [
-    {"id":1,"ime":"Luka Gligorević","cestitka":"Srecan rodjendan!"},
-    {"id":2,"ime":"Katarina Trbojević","cestitka":"Tomice gej(m) masteru, srećan ti 25. rođendan!<br/> Sada si na 25.levelu svog života i ide ti sasvim solidno.<br/> Želim ti da u narednoj godini dodatno buffuješ sve svoje skillse i da naletiš na mnogo pogodnih prilika koje će ti dati BIG vetar u leđa da kreneš putem koji si sam sebi zacrtao.<br/> Isto tako želim ti puno hrabrosti i sreće kada naletiš na bilo kakav obstacle i jako jako puno progresa i character developmenta.<br/> A za to su ti potrebni i odgovarajući klanovi ljudi koji će te podržavati u svakoj tvojoj zamisli.<br/> Želim da ti ljudi u koje ulažeš vreme i energiju guraju na vrh i da uvek umeju da ti naprave na licu osmeh.<br/> Uvek će biti momenata kada deluje kao da stvari stagniraju ali u tim momentima ne zaboravi da su ovi ljudi upravo tu za tebe 🧡 Želim da ti naredna godina bude godina puna novih XP-jeva i ona godina koje se sećaš sa osmehom i zbog koje ćeš biti ponosan na sebe.<br/> A, sada go and reach your full potential!!! ❤❤❤From the 💜,SkinnyChubbyBunny"}
-   
-   ]
-
- 
-
+    
+      {
+       "id": 1,
+       "ime": "Marko Mitrovic",
+       "cestitka": "Nagledao sam se ovih cestitki, ne mogu vise. Ziv bio Tomatino I nek je sa srecom"
+      },
+      {
+       "id": 2,
+       "ime": "Luka Gligorevic",
+       "cestitka": "Halo dobar dan Tomin Rodjendan ovde, pa nek je sa srecom. Svasta nesto sam hteo da kazem, ali ovi ljudi imaju bas lepe reci. Tako da te grlim kad te vidim, onako jako bas. "
+      },
+      {
+       "id": 3,
+       "ime": "Djordje Nemet",
+       "cestitka": "Srecan rodjendan! da postanes najbolji programator i potencijalan game dizajner, nastavis i uspes u svim kreativnim aktivnostima jer si takav taletanko, se naputujes i vise sledece godine nego ove, poy i vidjamo se sutra 😘😘"
+      },
+      {
+       "id": 4,
+       "ime": "Milos Savic",
+       "cestitka": "Baby, don't go (Don't go) I'm begging, don't leave (Please, don't leave) My heart no more (Heart no more) Please, stand by me (Stand by me) Srecan rodjendan, volim te!"
+      },
+      {
+       "id": 5,
+       "ime": "Marta Andric",
+       "cestitka": "Srećan rođendan sunce moje slatko🌞 Želim ti da postaneš najbolji game designer i programer, a dečko već jesi❤️"
+      },
+      {
+       "id": 6,
+       "ime": "Marija Rajevic",
+       "cestitka": "hoOooOoneey, happy birthday! Today a true queen was born and I’m so happy to be in your court. Cheers to many more laughs, adventures and best memories with you! 🥳🥂🎂👑💃🏻🕺🏻🪩💕💘💋⛰🗺"
+      },
+      {
+       "id": 7,
+       "ime": "Olga Pantic",
+       "cestitka": "Bebeee srecan rodjendan, zelim ti mnogo srece, ljubavi, uspeha, radosti, zurkica i putovanja 🫶🏻 Luv ya 💙💙💙"
+      },
+      {
+       "id": 8,
+       "ime": "Dunja  Stefanovic",
+       "cestitka": "Tono srecan rodjendan! Da jos sto godina slayujemo svaku zurku gde se pojavimo! B3c@uSe T0n!gHt w!LL b3 tHe N!gHt tH@t I w!LL F@LL F0r uuuuuu Ljubi te Dulja!"
+      },
+      {
+       "id": 9,
+       "ime": "Aleksa Krnjavcevic",
+       "cestitka": "Tonoooo, my dood!Srecan rodjendan! okaaaaaaay lessgo Puno uspeha u progra miranju!!!"
+      },
+      {
+       "id": 10,
+       "ime": "Bozidar  Petrovic",
+       "cestitka": "Tomo ljubavi moja, srećan rođendan, da si mi živ, zdrav lep, nasmejan i uspešan još punooo godina(kao i do sad) i da uvek u životu budeš okružen ljudima koji te vole 😊❤️🖕🏻❤️😊"
+      },
+      {
+       "id": 11,
+       "ime": "Nemanja  Radovanovic",
+       "cestitka": "Tomice bebice srecan rodjendan ti zeli Charmzi!!"
+      },
+      {
+       "id": 12,
+       "ime": "Nemanja  Milovanovic",
+       "cestitka": "Tono brate srecan rodjendan, sve najlepse"
+      },
+      {
+       "id": 13,
+       "ime": "Miodrag Milojevic",
+       "cestitka": "Tomo srce moje, zelim ti sve najbolje i najlepse! Nek ti je najsrecniji rodjendan do sad!"
+      },
+      {
+       "id": 14,
+       "ime": "Teodora Gligorevic",
+       "cestitka": "Srećan rođendan Tomice!! Nadam se da ćeš plesati još dugo i da ćeš biti loš u igricama još duže ❤️"
+      },
+      {
+       "id": 15,
+       "ime": "Anja Matic",
+       "cestitka": "srećan rođendan tomtom! mnogo stvari si već sam sebi ostvario tako da nisam sigurna šta da ti poželim osim olakšan put do čega još želiš da stigneš, puno ljubavi i divnih ljudi a i neki jači keš ne bi bio na odmet da možemo na neke festivalčiće (red bi bio) neki front row? duh bro gasss 🤍 uživaj!"
+      },
+      {
+       "id": 16,
+       "ime": "Elena Todorovic",
+       "cestitka": "Tomo, srećan rođendan. Želim ti da uvek budeš takav kakav sada jesi. Draga osoba, puna kreativnosti i želje za nečim novim. Ostani odan sebi i svojim snovima. Želim ti takođe puno ljubavi, jer na kraju dana to je jedino što je važno. Drago mi je što baš sa  tobom mogu da podelim ceo kreativni proces stvaranja i što si baš ti moj partner u plesu. Sve najlepše za tebe, Tomo.💖💖"
+      },
+      {
+       "id": 17,
+       "ime": "Ilijana Gligorevic",
+       "cestitka": "Sve najlepše ti želim od 💙.Ljubi majka²  🎉🎂😘"
+      },
+      {
+       "id": 18,
+       "ime": "Vladan Palecek",
+       "cestitka": "Srećan rođenadan, Taja (Mora nežnost prema sinu!)"
+      },
+      {
+       "id": 19,
+       "ime": "Ljubica Palecek",
+       "cestitka": "Mir u dusi, zdravlje u telu, spokoj u dusi, uspeh u radu i ljubav u srcu.. SRECAN 25.!!!😍"
+      },
+      {
+       "id": 20,
+       "ime": "Dragana Palecek",
+       "cestitka": "\"If I was dying on my knees, you would be the one to rescue me,\nIf you were drowning out at sea, I'd give you my lungs so you could breathe,\nI've got you brother ❣️\"\nI naravno srećan rođendan Tomo 🎉🥳🍾🥂"
+      },
+      {
+       "id": 21,
+       "ime": "Katarina Trbojevic",
+       "cestitka":"Tomice gej(m) masteru, srećan ti 25. rođendan!<br/> Sada si na 25.levelu svog života i ide ti sasvim solidno.<br/> Želim ti da u narednoj godini dodatno buffuješ sve svoje skillse i da naletiš na mnogo pogodnih prilika koje će ti dati BIG vetar u leđa da kreneš putem koji si sam sebi zacrtao.<br/> Isto tako želim ti puno hrabrosti i sreće kada naletiš na bilo kakav obstacle i jako jako puno progresa i character developmenta.<br/> A za to su ti potrebni i odgovarajući klanovi ljudi koji će te podržavati u svakoj tvojoj zamisli.<br/> Želim da ti ljudi u koje ulažeš vreme i energiju guraju na vrh i da uvek umeju da ti naprave na licu osmeh.<br/> Uvek će biti momenata kada deluje kao da stvari stagniraju ali u tim momentima ne zaboravi da su ovi ljudi upravo tu za tebe 🧡 Želim da ti naredna godina bude godina puna novih XP-jeva i ona godina koje se sećaš sa osmehom i zbog koje ćeš biti ponosan na sebe.<br/> A, sada go and reach your full potential!!! ❤❤❤ </br>From the 💜, SkinnyChubbyBunny"
+      },
+      {
+       "id": 22,
+       "ime": "Vanja Drobnjak",
+       "cestitka": "Srecan rodjendan nesudjenom menadzeru skakaonice 💜💜💜"
+      },
+      {
+       "id": 23,
+       "ime": "Jana  Andric",
+       "cestitka": "Pčelo moja  zelim ti najsrecniji rodjendan ljubim te u oko lepo, uzivaj grli te tvoj ljeptir 💜"
+      },
+      {
+       "id": 24,
+       "ime": "Predrag  Dedic",
+       "cestitka": "Tomislave moj lepi, da ti je srećan dan začetka tvog. Da te prate dobre stvari ove godine i da ti se ostvari sve što želiš al pre svega da si mi zdrav i srećan ostalo ćemo kupiti lako ima Djape kurbli uvek ❤️"
+      },
+      {
+       "id": 25,
+       "ime": "Dora Ples",
+       "cestitka": "thom 💅🏿 srecan rodjendaaaan 👴 sve najlepse!! zelim ti da si uvek okruzen dobrim ljudima,plesom i da nastavis da radis to sto volis!! 👉👈💉🤭"
+      },
+      {
+       "id": 26,
+       "ime": "Vojisalva Cekerevac",
+       "cestitka": "Tomoo srecan rodjis!! Da bude puno djuskanja, kreative, escape rumovanja,  ___ (sve sto si pozeleo sebi (al obavezno pozeli nesto)), i da ti se sve to asap osvari!!! Hagčina, srecaaaan"
+      },
+      {
+       "id": 27,
+       "ime": "Sofija Barjaktarevic",
+       "cestitka": "Tomooo, srecan rodjendan ti zelim i sve sve najlepse!! puno srece zdravlja uspeha ljubavi i plesa naravno :) saljem ti pozdrave preko okeana i uzivaj danas🫶🏼🫶🏼🫶🏼"
+      },
+      {
+       "id": 28,
+       "ime": "Arina Ivanova",
+       "cestitka": "Tono srecan rodjendan! Da jos sto godina slayujemo svaku zurku gde se pojavimo! Toma, srećan rođendan 💙Special day, special person and special celebration. May all your dreams and desires come true in this coming year! Be your own light. Find your own way. And don't forget about bananas 🍌 Miss you soooo much !"
+      },
+      {
+       "id": 29,
+       "ime": "Ivana Ana Kum",
+       "cestitka": "Tomo, kralju densa i budući programeru, želimo ti srećan rođendan i sve najlepše! Budi nam živ i zdrav, čekamo te da zajedno guramo evropsku bedu)))) I da nastavimo tamo gde smo stali, između ostalog da završimo koreografiju za ostatak numera Silk Sonica ❤️ Jedni jedini i neponovljivi Grenobl Srbi"
+      },
+      {
+       "id": 30,
+       "ime": "Lusi Korobova",
+       "cestitka": "Tomislave, с Днём рождения!💌 Da postaneš ne samo uspešan programer i plesač (što rln već jesi, al neka se to širi i širi još koliko god ti to budeš želeo, a nadam se da ćeš otići jako daleko) nego i reditelj, i da ti se sve lične i kreativne mašte ispune😎 I da što pre gledamo Please Do Not Lean💘🥳🥳🥳 sve sve naj"
+      },
+      {
+       "id": 31,
+       "ime": "Ana Markovic",
+       "cestitka": "Dragi Tomo,Nekako mi je ironično što ulaziš u ovaj imaginaran svet odakle čitaš ove naše poruke. Nema rukopisa ili dodira sa papirom ali mislim da je nekako dobar kompromis, momenat nežnosti u ovom crnom staklu me dosta podseća na tebe. No, da je filozofiram mnogo, imaš jednu predivnu životnu energiju koja se retko kada viđa i drago mi je što sam te upoznala da bih je videla. Tako da ti želim da samo nastaviš, sa tom tvojom energijom da prikupljaš ljude i utičeš na događaje koje će vas sve dotaći ili\/i veseliti. Ono tipično smeh, muzika, pokret, ljubav… Sa tim srećan ti, a I svima nama koji te znamo i cenimo, rođendan!!! Nadam se da ćeš se lepo provesti :)Mnogo ljubavi i zagrljaja,Ana"
+      },
+      {
+       "id": 32,
+       "ime": "Julija Arzumanova",
+       "cestitka": "Dear Toma! I wish you to always stay positive, listen to good music, go on amazing trips and not break your legs on stage! Follow your dreams and... DONT WORRY BE HAPPY MAN DONT GIVE A FUCK ABOUT SHITTY PEOPLE YOU ARE THE KING YO! Love you kiss you (Ju)"
+      },
+      {
+       "id": 33,
+       "ime": "Ilija Santric",
+       "cestitka": "Tomo Srecan ti rodjendan! Da ti se ruke pozlate a hrana koju stvaras sa vremonom bude sve ukusnija! "
+      }
+     ]
